56e920d7197104f713a30328f31851200e5d262b
Shrinidhi <ballalshrinidhi@gmail.com>
2024-07-29 01:09:33 +0530

    fifth commit

--- Managers/Manager.py
+++ Managers/Manager.py
@@ -14,18 +14,25 @@ if __name__ == "__main__":
     user = int(sys.argv[1])
     order = sys.argv[2]
     commandinput = int(sys.argv[3])
+    # Machine Assembly Manager
     if user==1:
         commandManager(0,1,[['Engine',['Battery','Wire','Piston']],['Frame',['UpperPlate','BottomPlate']]],order,commandinput)
+    # Engine Assembly Manager
     elif user==2:
         commandManager(1,2,[['BikeFrame',['Engine','Frame']]],order,commandinput)
+    # Painted Assembly Manager
     elif user==3:
         commandManager(2,3,[['BikeFramePainted',['Paint','BikeFrame']]],order,commandinput)
+    # Vehicle Assembly Manager
     elif user==4:
         commandManager(3,4,[['Bike',['Tyre','Screw','BikeFramePainted']]],order,commandinput)
+    # Store Manager
     elif user==5:
         storeManager(order)
+    # Initialising files
     elif user==6:
         createmessenger()
+    # Production Manager
     else:
         setTarget(order)
 
